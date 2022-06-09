473e4cd3dcae398eb99cb1b5049ef670ab0ede60
pedro-ernesto <silveira-pedro@outlook.com>
2022-06-09 19:33:25 -0300

    created department and subject entities

---
+++ src/modules/professors/entities/Department.ts
@@ -0,0 +1,38 @@
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  JoinColumn,
+  ManyToOne,
+  PrimaryColumn,
+} from "typeorm";
+import { v4 as uuidV4 } from "uuid";
+
+import { Institution } from "../../institutions/entities/Institution";
+
+@Entity("departments")
+class Department {
+  @PrimaryColumn()
+  id?: string;
+
+  @Column()
+  name: string;
+
+  @Column()
+  institution_id: string;
+
+  @ManyToOne(() => Institution)
+  @JoinColumn({ name: "institution_id" })
+  institution: Institution;
+
+  @CreateDateColumn()
+  created_at: Date;
+
+  constructor() {
+    if (!this.id) {
+      this.id = uuidV4();
+    }
+  }
+}
+
+export { Department };
